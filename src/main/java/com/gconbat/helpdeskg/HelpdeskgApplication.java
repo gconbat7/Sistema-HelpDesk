f08a8e683296fc47132ff5f5d02fa7b4531f54b3
gconbat7 <gidocode7@gmail.com>
2024-10-06 12:38:31 -0300

    Início do projeto sistema de HelpDesk

---
+++ src/main/java/com/gconbat/helpdeskg/HelpdeskgApplication.java
@@ -0,0 +1,13 @@
+package com.gconbat.helpdeskg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpdeskgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpdeskgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gconbat/helpdeskg/HelpdeskgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gconbat.helpdeskg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdeskgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
